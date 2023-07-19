2caf5c1e7befa7e0ff9d19d873745664af381a90
Jim Schutz <jim@vfade.com>
2023-07-18 21:01:03 -0500

    Add icloud directory

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
 BROWSER_NAME = "Google Chrome"
-TB_PATH = os.path.expanduser("~")
+TB_PATH = os.path.expanduser("~") # User's home directory
+# TB_PATH = os.path.expanduser(
+#     "~/Library/Mobile%20Documents/com~apple~CloudDocs"
+# )  # User's iCloud directory
 TB_DIR = ".textbundles"
 TB_TYPES = ["task", "project", "meeting", "note"]
 TB_INFO = {
